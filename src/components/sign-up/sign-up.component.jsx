b338e3c6eadee4ebcde64f45098f5bf367c70316
Puneet Singh 4 <puneet3011@gmail.com>
2020-03-05 22:37:26 -0500

    responsive design for signin-signup

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -49,7 +49,7 @@ class SignUp extends React.Component {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
       <div className="sign-up">
-        <h2 className="title">I do not have a account</h2>
+        <span className="title">I do not have a account</span>
         <span>Sign up with your email and password</span>
         <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
